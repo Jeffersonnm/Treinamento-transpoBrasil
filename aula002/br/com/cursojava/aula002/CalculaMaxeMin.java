f254e15558744f3c51bc04471b4613b88908aab5
jeffersonnm <jeffersonnazariomendes@gmail.com>
2018-09-22 11:42:06 -0300

    Exercicios da manhã

---
+++ aula002/br/com/cursojava/aula002/CalculaMaxeMin.java
@@ -0,0 +1,26 @@
+package br.com.cursojava.aula002;
+
+import java.util.Scanner;
+
+public class CalculaMaxeMin {
+	
+	double n1;
+	double n2;
+	double n3;
+	
+	public CalculaMaxeMin() {
+		n1 = pedeDados("Informe nota 1");
+		n2 = pedeDados("Informe nota 2");
+		n3 = pedeDados("Informe nota 3");
+		System.out.printf("A maior nota é: %f\nA menor nota é: %f", Math.max(Math.max(n1, n3), Math.max(n2, n3)), Math.min(Math.min(n1, n3), Math.min(n2, n3)));
+	}
+	
+	double pedeDados(String mensag) {
+		System.out.println(mensag);
+		return new Scanner(System.in).nextDouble();
+	}
+	
+	public static void main(String[] args) {
+		CalculaMaxeMin c = new CalculaMaxeMin();
+	}
+}
---
+++ aula002/br/com/cursojava/aula002/MediaPonderada.java
@@ -0,0 +1,32 @@
+package br.com.cursojava.aula002;
+
+import java.util.Scanner;
+
+public class MediaPonderada {
+	
+	double nota1;
+	double nota2;
+	double nota3;
+	double peso1;
+	double peso2;
+	double peso3;
+	
+	public MediaPonderada() {
+		nota1 = pedeDados("Informe a nota 1");
+		nota2 = pedeDados("Informe a nota 2");
+		nota3 = pedeDados("Informe a nota 3");
+		peso1 = 2;
+		peso2 = 3;
+		peso3 = 5;
+		System.out.printf("A media pondera é igual a: %.2f", ((nota1*peso1) + (nota2*peso2) + (nota3*peso3))/(peso1+peso2+peso3));
+	}
+	
+	double pedeDados(String mensag) {
+		System.out.println(mensag);
+		return new Scanner(System.in).nextDouble();
+	}
+
+	public static void main(String[] args) {
+		MediaPonderada m = new MediaPonderada();
+	}
+}
